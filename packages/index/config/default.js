94691dcf634556317051ddd1c1753afa4857b29f
Nirmal Gupta <nirmalk.gupta@gmail.com>
2020-04-16 23:00:06 -0400

    index for BTC-USD

--- packages/index/config/default.js
+++ packages/index/config/default.js
@@ -1,81 +1,74 @@
-const average = (a, b) => ((a - 0) + (b - 0)) / 2
-const kyberPrice = (_) => {
-  const dai = _.data.filter(price => price.pair === "ETH_DAI")
-  if (dai.length !== 1) return undefined
-  return average(dai[0].current_bid, dai[0].current_ask)
-}
-
-const fatbtcPrice = (_) => {
-  let ticker = _.data.ethdai_ticker;
-  if (!ticker) return undefined
-  return average(ticker.bis1[0], ticker.ask1[0])
-}
-
-
+const sec      = 1000
+const min      = 60 * sec
 module.exports = {
-  "logExternalPrice": true,
-  "expiryTime": 6,
-  "port": 8090,
-  "startupDelay": 10000,
-  "components": {
-    "ETH-DAI": {
-      "minExternalProviders": 5,
-      "ticksize": 3,
-      "topic": "leverj-index#ETHDAI",
-      "providers": [
+  logExternalPrice: true,
+  expiryTime:       6,
+  port:             8090,
+  startupDelay:     10 * sec,
+  components:       {
+    BTCUSD: {
+      minExternalProviders: 5,
+      ticksize:             1,
+      topic:                "index#BTCUSD",
+      providers:            [
         {
-          "name": "bitfinex",
-          "url": () => "https://api.bitfinex.com/v1/pubticker/daieth",
-          price: (_) => average(_.bid, _.ask),
-          "frequency": 5000,
-          "inverse": true
+          name:      "bitfinex",
+          url:       () => "https://api-pub.bitfinex.com/v2/ticker/tBTCUSD",
+          price:     _ => _[6],
+          frequency: 10 * sec,
+          inverse:   false
         },
         {
-          "name": "bancor",
-          "url": () => "https://api.bancor.network/0.1/currencies/ETH/ticker?fromCurrencyCode=DAI&displayCurrencyCode=DAI",
-          price: (_) => _.data.price - 0,
-          "frequency": 1000,
-          "inverse": false
+          name:      "currency.com",
+          url:       () => `https://api-adapter.backend.currency.com/api/v1/klines?symbol=BTC/USD&interval=1m&limit=1&start=${Date.now()}`,
+          price:     _ => parseFloat(_[0][4]),
+          frequency: 10 * sec,
+          inverse:   false
         },
         {
-          "name": "coinmarketcap",
-          "url": () => "https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=DAI",
-          price: (_) => _[0].price_dai - 0,
-          "frequency": 1000,
-          "inverse": false
-        }, {
-          "name": "hitbtc",
-          "url": () => "https://api.hitbtc.com/api/2/public/ticker/ETHDAI",
-          price: (_) => average(_.bid, _.ask),
-          "frequency": 1000,
-          "inverse": false
+          name:      "BW.com",
+          url:       () => "https://www.bw.com/api/data/v1/klines?marketId=281&type=1M&dataSize=1",
+          price:     _ => parseFloat(_.datas[0][7]),
+          frequency: 10 * sec,
+          inverse:   false
         },
         {
-          "name": "kucoin",
-          "url": () => "https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=ETH-DAI",
-          price: (_) => average(_.data.bestBid, _.data.bestAsk),
-          "frequency": 1000,
-          "inverse": false
-        }, {
-          "name": "kyber",
-          "url": () => "https://api.kyber.network/market",
-          price: kyberPrice,
-          "frequency": 1000,
-          "inverse": true
-        }, {
-          "name": "fatbtc",
-          "url": () => `https://www.fatbtc.us/m/allticker/${Date.now()}`,
-          price: fatbtcPrice,
-          "frequency": 1000,
-          "inverse": true
+          name:      "zbg.com",
+          url:       () => "https://kline.zbg.com/api/data/v1/klines?marketName=btc_usdt&type=1M&dataSize=1",
+          price:     _ => parseFloat(_.datas[0][7]),
+          frequency: 10 * sec,
+          inverse:   false
         },
+        {
+          name:      "hitbtc",
+          url:       () => "https://api.hitbtc.com/api/2/public/ticker/BTCUSD",
+          price:     _ => parseFloat(_.last),
+          frequency: 10 * sec,
+          inverse:   false
+        },
+        {
+          name:      "binance",
+          url:       () => `https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=1&startTime=${Date.now() - Date.now() % min}`,
+          price:     _ => parseFloat(_[0][4]),
+          frequency: 10 * sec,
+          inverse:   false
+        },
+        {
+          name:      "houbi",
+          url:       () => `https://api.huobi.pro/market/history/kline?symbol=btcusdt&period=1min&size=1`,
+          price:     _ => parseFloat(_.data[0].close),
+          frequency: 10 * sec,
+          inverse:   false
+        },
+
       ]
     }
   },
-  "enabled": [
+  enabled:          [
     {
-      "currency": "ETH-DAI",
-      "providers": ["bitfinex", "bancor", "coinmarketcap", "hitbtc", "kucoin", "kyber", "fatbtc"]
+      currency:  "BTCUSD",
+      providers: ["bitfinex", "currency.com", "BW.com", "zbg.com", "hitbtc", "binance", "houbi"]
+      // providers: ["binance"]
     }
   ]
 }
